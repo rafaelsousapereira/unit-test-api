6db1ab9fee001bd335e0103560f415b23c30bf05
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-10-19 16:28:06 -0300

    feat: initial project

---
+++ src/main/java/br/com/udemy/rafael/unittest/UnitTestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.rafael.unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/udemy/rafael/unittest/UnitTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.rafael.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
