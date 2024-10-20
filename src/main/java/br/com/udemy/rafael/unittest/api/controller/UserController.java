a2040fe08095cc9ba0bf614f7dc38bd4edde2127
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-10-20 16:11:43 -0300

    feat: Add user class controller findById

--- src/main/java/br/com/udemy/rafael/unittest/api/controller/UserController.java
+++ src/main/java/br/com/udemy/rafael/unittest/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package br.com.udemy.rafael.unittest.api.controller;
 
+import br.com.udemy.rafael.unittest.api.controller.sevice.UserService;
 import br.com.udemy.rafael.unittest.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-         return ResponseEntity.ok().body(new User(1, "Rafael", "rafael@mail.com", "1234"));
+        var user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
